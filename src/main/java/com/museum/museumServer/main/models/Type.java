fc513f1a75308fbacea2a4b2d8d78d7bb1458749
JORGE LUIS PALOMARES RUIZ <6001224@insags.com>
2019-04-03 18:01:11 +0200

    actualizar

--- src/main/java/com/museum/museumServer/main/models/Type.java
+++ src/main/java/com/museum/museumServer/main/models/Type.java
@@ -1,4 +1,23 @@
 package com.museum.museumServer.main.models;
 
-public class Type {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Document(collection = "type")
+public class Type implements Serializable {
+
+    @Id
+    @NotNull
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
