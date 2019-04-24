701af73e43daea442826d8e2b2b2dbbbc3ea41de
tomsagu <tsayago1997@gmail.com>
2019-04-24 19:35:48 +0200

    Update DocumentRepository.java

--- src/main/java/com/museum/museumServer/main/repository/DocumentRepository.java
+++ src/main/java/com/museum/museumServer/main/repository/DocumentRepository.java
@@ -2,10 +2,20 @@ package com.museum.museumServer.main.repository;
 
 import com.museum.museumServer.main.models.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.io.Serializable;
+import java.util.List;
 
+@CrossOrigin
 @Repository
-public interface DocumentRepository extends MongoRepository<Document, Serializable> {
+public interface DocumentRepository extends MongoRepository<Document, Serializable>{
+
+
+    List<Document> findByNameLikeOrTextLike(@Param("word")String word, @Param("word1")String word1);
+
 }
